4e0b5b4c913b15be6f9a07caa313a8b8a24a3acb
Vansh Rastogi <rastogiv@myumanitoba.ca>
2023-08-23 17:54:53 -0500

    streams

---
+++ streams.js
@@ -0,0 +1,11 @@
+const {createReadStream} = require('fs')
+
+const stream = createReadStream('./new1.txt', {highWaterMark:90000, encoding:'utf8'})
+
+stream.on('data', (result) => {
+console.log(result)
+})
+
+stream.on('error' ,(error) => {
+    console.log(error)
+})
